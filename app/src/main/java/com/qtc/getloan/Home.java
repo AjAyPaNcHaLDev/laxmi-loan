2b7abb631007f6294d8fcaf3cc57feb79ef6a814
AjAyPaNcHaLDev <ap8570091@gmail.com>
2022-08-08 00:43:55 +0530

    adding onClick and intent to all post

--- app/src/main/java/com/qtc/getloan/Home.java
+++ app/src/main/java/com/qtc/getloan/Home.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 
@@ -12,6 +14,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
@@ -19,7 +23,19 @@ import android.widget.Toast;
 
 public class Home extends Fragment {
 
-Button gettingStartBtn;
+            Button gettingStartBtn,
+            loanAgentProperty,
+            newHomeLoanBtn,
+            transferExistingLoan;
+
+            ImageView caseLoanBanner,
+            personalLoanBanner,
+            mutualFundBanner,
+            homeLoanBanner;
+
+            ConstraintLayout healthCard;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -29,13 +45,87 @@ Button gettingStartBtn;
             @Override
             public void onClick(View view) {
                 Intent i =new Intent(getActivity().getApplicationContext(),MainActivity2.class);
+                i.putExtra("title","Getting Start");
                 getActivity().startActivity(i);
             }
         });
 
+        healthCard=view.findViewById(R.id.healthCard);
+        healthCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(getActivity().getApplicationContext(),AdsActivity.class);
+                i.putExtra("title","Health");
+                getActivity().startActivity(i);
+            }
+        });
 
+        caseLoanBanner=view.findViewById(R.id.caseLoanBanner);
+        caseLoanBanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(getActivity().getApplicationContext(),MainActivity2.class);
+                i.putExtra("title","Cash Loan");
+                getActivity().startActivity(i);
+            }
+        });
 
-return view;
+        mutualFundBanner=view.findViewById(R.id.mutualFundBanner);
+        mutualFundBanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(getActivity().getApplicationContext(),MainActivity2.class);
+                i.putExtra("title","Mutual Fund");
+                getActivity().startActivity(i);
+            }
+        });
+        personalLoanBanner=view.findViewById(R.id.personalLoanBanner);
+        personalLoanBanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(getActivity().getApplicationContext(),MainActivity2.class);
+                i.putExtra("title","Personal Loan");
+                getActivity().startActivity(i);
+            }
+        });
+        homeLoanBanner=view.findViewById(R.id.homeLoanBanner);
+        homeLoanBanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(getActivity().getApplicationContext(),MainActivity2.class);
+                i.putExtra("title","Home Loan");
+                getActivity().startActivity(i);
+            }
+        });
+        loanAgentProperty=view.findViewById(R.id.loanAgentProperty);
+        loanAgentProperty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(getActivity().getApplicationContext(),MainActivity2.class);
+                i.putExtra("title","Loan Agent Property");
+                getActivity().startActivity(i);
+            }
+        });
+        transferExistingLoan=view.findViewById(R.id.transferExistingLoan);
+        transferExistingLoan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(getActivity().getApplicationContext(),MainActivity2.class);
+                i.putExtra("title","Transfer Existing Loan");
+                getActivity().startActivity(i);
+            }
+        });
+        newHomeLoanBtn=view.findViewById(R.id.newHomeLoanBtn);
+        newHomeLoanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(getActivity().getApplicationContext(),MainActivity2.class);
+                i.putExtra("title","New Home Loan");
+                getActivity().startActivity(i);
+            }
+        });
+
+        return view;
 
 
     }
--- app/src/main/java/com/qtc/getloan/MainActivity2.java
+++ app/src/main/java/com/qtc/getloan/MainActivity2.java
@@ -2,13 +2,20 @@ package com.qtc.getloan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity2 extends AppCompatActivity {
-
+String title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+            setContentView(R.layout.activity_main2);
+
+        Intent intent=getIntent();
+    title=intent.getStringExtra("title");
+    getSupportActionBar().setTitle(title);
     }
 }
