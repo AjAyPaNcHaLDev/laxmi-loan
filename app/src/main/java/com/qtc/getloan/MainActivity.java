b232dfe6930dd2923f1689f512d5cbf432685c34
AjAyPaNcHaLDev <ap8570091@gmail.com>
2022-08-07 13:12:23 +0530

    fragment layout added and working properly

--- app/src/main/java/com/qtc/getloan/MainActivity.java
+++ app/src/main/java/com/qtc/getloan/MainActivity.java
@@ -1,25 +1,17 @@
 package com.qtc.getloan;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.qtc.getloan.fragments.Fragment1;
-import com.qtc.getloan.fragments.Fragment2;
-import com.qtc.getloan.fragments.Fragment3;
 
 
 public class MainActivity extends AppCompatActivity {
-//Button gettingStartBtn;
-    BottomNavigationView bottomNavigationView;
-
+BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,43 +20,30 @@ public class MainActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Fragment1()).commit();
-
-//        gettingStartBtn=findViewById(R.id.GettingStartBtn);
-//        gettingStartBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//Intent i =new Intent(MainActivity.this, AdsActivity.class);
-//startActivity(i);
-//
-//            }
-//        });
-        bottomNavigationView=findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new Home()).commit();
+       bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onNavigationItemReselected(@NonNull MenuItem item) {
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment fragment=null;
-                 switch (item.getItemId()){
-                     case R.id.page_1:
-                         fragment=new Fragment1();
-
-                         break;
-
-                         case R.id.page_2:
-                             fragment=new Fragment2();
-                         break;
-
-                         case R.id.page_3:
-                             fragment=new Fragment3();
-                         break;
+                switch (item.getItemId()) {
+                    case R.id.page_1:
+                        fragment = new Home();
+                        break;
+                    case R.id.page_2:
+                        fragment = new Dashboard();
+                        break;
+                    case R.id.page_3:
+                        fragment = new LogIn();
+                        break;
 
 
-                 }
-                 getSupportFragmentManager().beginTransaction().replace(R.id.bottom_navigation,fragment).commit();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                return true;
             }
         });
-
-
 
     }
 }
